c898fb870ad25a41e805ec8c90b6ccbdeda161e0
david hernandez <davidfelipehernandez1@gmail.com>
2022-09-26 22:27:28 -0500

    ar_operations done

--- src/ar_op_fn.rs
+++ src/ar_op_fn.rs
@@ -1,36 +1,68 @@
 use std::io;
 
+fn separator_line() {
+    for _ in 0..=50 {
+        print!("-");
+    }
+    println!()
+}
+//entry point cli
+pub fn ar_op() -> bool {
+    let mut yes_no: String = String::new();
+
+    println!("Want to do some aritmetic operations? (Y/N): ");
+
+    //gets input
+    io::stdin().read_line(&mut yes_no).expect("Expect (Y/N)");
+    let yes_no_lower: char = yes_no.to_lowercase().chars().next().unwrap();
+
+    if yes_no_lower == 'y' {
+        true
+    } else {
+        false
+    }
+}
+
 // This ask for what arithmetic you want
 pub fn ar_op_fn() {
-    println!("sds");
+    separator_line();
+
     loop {
         println!("What you want to do?");
         println!("1. (+)");
         println!("2. (-)");
         println!("3. (*)");
         println!("4. (/)");
+        println!("5. Exit");
 
         let mut select: String = String::new();
         io::stdin().read_line(&mut select).expect("Expect a number");
-        let select: u32 = match select.trim().parse() {
+        let select: i32 = match select.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
 
-        if select > 0 || select < 5 {
-        } else {
-            println!("Your option {} isn't available", select);
+        if select <= 0 || select >= 5 {
+            separator_line();
+            println!("You exit the program :D.");
             std::process::exit(0)
         }
+
+        if select > 1 || select < 5 {
+            let res: i32 = ar_op_fn_operations(select);
+            println!("result = {}", res);
+        }
+        separator_line();
     }
 }
 
 //This does the different arithmetic operations
-pub fn ar_op_fn_operations(option: i32) -> i64 {
+pub fn ar_op_fn_operations(option: i32) -> i32 {
     let mut first_num: String = String::new();
     let mut second_num: String = String::new();
-    let mut res: i64 = 0;
+    let res: i32;
 
+    println!("Enter your numbers");
     io::stdin()
         .read_line(&mut first_num)
         .expect("Expect first number");
@@ -38,11 +70,11 @@ pub fn ar_op_fn_operations(option: i32) -> i64 {
         .read_line(&mut second_num)
         .expect("Expect first number");
 
-    let first_num: i64 = match first_num.trim().parse() {
+    let first_num: i32 = match first_num.trim().parse() {
         Ok(num) => num,
         Err(_) => std::process::exit(0),
     };
-    let second_num: i64 = match second_num.trim().parse() {
+    let second_num: i32 = match second_num.trim().parse() {
         Ok(num) => num,
         Err(_) => std::process::exit(0),
     };
